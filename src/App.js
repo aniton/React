f5357bc2981ec75e4f1d8dae69df4e167e6080be
Anita Soloveva <nit-sol@mail.ru>
2020-07-15 12:30:46 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Component } from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Main from './components/MainComponent';
+import './App.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfigureStore } from './redux/configureStore'
+const store = ConfigureStore();
+class App extends Component {
+
+render() {
+  return (
+    <Provider store={store}>
+    <BrowserRouter>
+    <div>
+    <Main />
+    </div>
+      </BrowserRouter>
+      </Provider>
+  );
+}
+}
+export default App;
